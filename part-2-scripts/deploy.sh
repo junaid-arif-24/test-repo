c22b455d65e046bdffb7b3dbff4210031eda1abf
junaid-arif-24 <mohdjunaid7078@gmail.com>
2023-08-29 17:17:58 +0530

    Modified deploying script

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
-cd Week-5-with-axios-and-backend
- git pull origin master
- cd server
- pm2 kill
- pm2 start index.js
+cd week-9
+git pull origin master
+cd server
+
+# Add the pm2 directory to the PATH here
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin/pm2
+
+pm2 kill
+pm2 start index.js
